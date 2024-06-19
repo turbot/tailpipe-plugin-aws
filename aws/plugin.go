2a695317465075ddf003d179e58dfdb41c936875
kai <kai@turbot.com>
2024-06-19 16:24:04 +0100

    working on it

--- aws/plugin.go
+++ aws/plugin.go
@@ -28,6 +28,28 @@ func (t *Plugin) Collect(req *proto.CollectRequest) error {
 }
 
 func (t *Plugin) doCollect(req *proto.CollectRequest) {
+	//
+	//"sources": {
+	//	"my_file": {
+	//		"name": "my_file",
+	//			"plugin": "file",
+	//			"config": {
+	//			"path": "/Users/nathan/src/play-duckdb/2023/02",
+	//				"extensions": []
+	//}
+	//}
+	//},
+	//"collections": {
+	//"my_aws_log": {
+	//"plugin": "aws_cloudtrail_log",
+	//"name": "my_aws_log",
+	//"source": "my_file"
+	//}
+	//}
+	// tactical
+	//create source and collection
+	source := &AwsS3BucketSource{}
+	collection := &AwsCloudTrailLogCollection{}
 
 	// started hook
 	t.OnStarted(req)
