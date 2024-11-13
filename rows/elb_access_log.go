ff5fe503cf0ceb402a1cfcb14bc6dc1d499fd02f
kai <kai@turbot.com>
2024-11-13 12:43:34 +0700

    remove unneeded column tags

--- rows/elb_access_log.go
+++ rows/elb_access_log.go
@@ -12,38 +12,38 @@ import (
 type ElbAccessLog struct {
 	enrichment.CommonFields
 
-	Type                   string    `json:"type" column:"type"`
-	Timestamp              time.Time `json:"timestamp" column:"timestamp"`
-	Elb                    string    `json:"elb" column:"elb"`
-	ClientIP               string    `json:"client_ip" column:"client"`
-	ClientPort             int       `json:"client_port" column:"client_port"`
-	TargetIP               *string   `json:"target_ip,omitempty" column:"target"`
-	TargetPort             int       `json:"target_port,omitempty" column:"target_port"`
-	RequestProcessingTime  float64   `json:"request_processing_time" column:"request_processing_time"`
-	TargetProcessingTime   float64   `json:"target_processing_time" column:"target_processing_time"`
-	ResponseProcessingTime float64   `json:"response_processing_time" column:"response_processing_time"`
-	ElbStatusCode          *int      `json:"elb_status_code,omitempty" column:"elb_status_code"`
-	TargetStatusCode       *int      `json:"target_status_code,omitempty" column:"target_status_code"`
-	ReceivedBytes          *int64    `json:"received_bytes" column:"received_bytes"`
-	SentBytes              *int64    `json:"sent_bytes" column:"sent_bytes"`
-	Request                string    `json:"request" column:"request"`
-	UserAgent              string    `json:"user_agent" column:"user_agent"`
-	SslCipher              string    `json:"ssl_cipher" column:"ssl_cipher"`
-	SslProtocol            string    `json:"ssl_protocol" column:"ssl_protocol"`
-	TargetGroupArn         string    `json:"target_group_arn" column:"target_group_arn"`
-	TraceID                string    `json:"trace_id" column:"trace_id"`
-	DomainName             string    `json:"domain_name" column:"domain_name"`
-	ChosenCertArn          string    `json:"chosen_cert_arn" column:"chosen_cert_arn"`
-	MatchedRulePriority    int       `json:"matched_rule_priority" column:"matched_rule_priority"`
-	RequestCreationTime    time.Time `json:"request_creation_time" column:"request_creation_time"`
-	ActionsExecuted        string    `json:"actions_executed" column:"actions_executed"`
-	RedirectURL            *string   `json:"redirect_url,omitempty"	column:"redirect_url"`
-	ErrorReason            *string   `json:"error_reason,omitempty" column:"error_reason"`
-	TargetList             *string   `json:"target_list,omitempty" column:"target_list"`
-	TargetStatusList       *string   `json:"target_status_list,omitempty" column:"target_status_list"`
-	Classification         *string   `json:"classification,omitempty" column:"classification"`
-	ClassificationReason   *string   `json:"classification_reason,omitempty" column:"classification_reason"`
-	ConnTraceID            string    `json:"conn_trace_id" column:"conn_trace_id"`
+	Type                   string    `json:"type"`
+	Timestamp              time.Time `json:"timestamp"`
+	Elb                    string    `json:"elb"`
+	ClientIP               string    `json:"client_ip"`
+	ClientPort             int       `json:"client_port"`
+	TargetIP               *string   `json:"target_ip,omitempty"`
+	TargetPort             int       `json:"target_port,omitempty"`
+	RequestProcessingTime  float64   `json:"request_processing_time"`
+	TargetProcessingTime   float64   `json:"target_processing_time"`
+	ResponseProcessingTime float64   `json:"response_processing_time"`
+	ElbStatusCode          *int      `json:"elb_status_code,omitempty"`
+	TargetStatusCode       *int      `json:"target_status_code,omitempty"`
+	ReceivedBytes          *int64    `json:"received_bytes"`
+	SentBytes              *int64    `json:"sent_bytes"`
+	Request                string    `json:"request"`
+	UserAgent              string    `json:"user_agent"`
+	SslCipher              string    `json:"ssl_cipher"`
+	SslProtocol            string    `json:"ssl_protocol"`
+	TargetGroupArn         string    `json:"target_group_arn"`
+	TraceID                string    `json:"trace_id"`
+	DomainName             string    `json:"domain_name"`
+	ChosenCertArn          string    `json:"chosen_cert_arn"`
+	MatchedRulePriority    int       `json:"matched_rule_priority"`
+	RequestCreationTime    time.Time `json:"request_creation_time"`
+	ActionsExecuted        string    `json:"actions_executed"`
+	RedirectURL            *string   `json:"redirect_url,omitempty"`
+	ErrorReason            *string   `json:"error_reason,omitempty"`
+	TargetList             *string   `json:"target_list,omitempty"`
+	TargetStatusList       *string   `json:"target_status_list,omitempty"`
+	Classification         *string   `json:"classification,omitempty"`
+	ClassificationReason   *string   `json:"classification_reason,omitempty"`
+	ConnTraceID            string    `json:"conn_trace_id"`
 }
 
 func NewElbAccessLog() *ElbAccessLog {
