960a1d961d154276db719880ef47a88b672c5ee1
kai <kai@turbot.com>
2024-11-14 11:34:04 +0700

    remove factory initialisation from NewPlugin

--- aws/plugin.go
+++ aws/plugin.go
@@ -4,7 +4,6 @@ import (
 	"github.com/turbot/go-kit/helpers"
 	"github.com/turbot/tailpipe-plugin-aws/config"
 	"github.com/turbot/tailpipe-plugin-sdk/plugin"
-	"github.com/turbot/tailpipe-plugin-sdk/table"
 	// reference the table package to ensure that the tables are registered by the init functions
 	_ "github.com/turbot/tailpipe-plugin-aws/tables"
 )
@@ -20,14 +19,8 @@ func NewPlugin() (_ plugin.TailpipePlugin, err error) {
 		}
 	}()
 
-	//fmt.Println(tables.AlbAccessLogTable{})
 	p := &Plugin{
 		PluginImpl: plugin.NewPluginImpl("aws", config.NewAwsConnection),
-	}
-
-	// initialise table factory
-	if err := table.Factory.Init(); err != nil {
-		return nil, err
 	}
 
 	return p, nil
