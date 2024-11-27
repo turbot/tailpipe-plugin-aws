35453a69dd34c5aa15702591a74e3316a573b0b8
Graza <code@graza.io>
2024-11-27 13:42:37 +0000

    fix: cost_usage_log table parses

--- mappers/cost_usage_mapper.go
+++ mappers/cost_usage_mapper.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/csv"
 	"fmt"
-	"log/slog"
 	"strconv"
 	"strings"
 	"time"
@@ -27,60 +26,56 @@ func (c *CostAndUsageLogMapper) Identifier() string {
 }
 
 func (c *CostAndUsageLogMapper) Map(_ context.Context, a any) (*rows.CostAndUsageLog, error) {
-	// TODO FIX ME
-	slog.Debug(">> Inside Map")
-	csvData, ok := a.(string)
+	csvRow, ok := a.(string)
 	if !ok {
 		return nil, fmt.Errorf("input data is not of type string")
 	}
-	r := csv.NewReader(strings.NewReader(csvData))
+
+	r := csv.NewReader(strings.NewReader(csvRow))
 	r.TrimLeadingSpace = true
-	records, err := r.ReadAll()
+	row, err := r.Read()
 	if err != nil {
 		fmt.Println("Error reading CSV data:", err)
 		return nil, err
 	}
-	slog.Debug("<< records", "...", records)
-	var logs []*rows.CostAndUsageLog
-	for _, row := range records {
-		// TODO - find a better way of doing this parsing
-		// this works for now, since the csv columns are fixed and ordered
-		slog.Debug("<< row", "...", row)
-		log := &rows.CostAndUsageLog{
-			InvoiceID:              &row[0],
-			PayerAccountId:         &row[1],
-			LinkedAccountId:        &row[2],
-			RecordType:             &row[3],
-			RecordID:               &row[4],
-			BillingPeriodStartDate: parseTime(row[5]),
-			BillingPeriodEndDate:   parseTime(row[6]),
-			InvoiceDate:            parseTime(row[7]),
-			PayerAccountName:       &row[8],
-			LinkedAccountName:      &row[9],
-			TaxationAddress:        &row[10],
-			PayerPONumber:          &row[11],
-			ProductCode:            &row[12],
-			ProductName:            &row[13],
-			SellerOfRecord:         &row[14],
-			UsageType:              &row[15],
-			Operation:              &row[16],
-			RateId:                 &row[17],
-			ItemDescription:        &row[18],
-			UsageStartDate:         parseTime(row[19]),
-			UsageEndDate:           parseTime(row[20]),
-			UsageQuantity:          parseFloat(row[21]),
-			BlendedRate:            &row[22],
-			CurrencyCode:           &row[23],
-			CostBeforeTax:          parseFloat(row[24]),
-			Credits:                parseFloat(row[25]),
-			TaxAmount:              parseFloat(row[26]),
-			TaxType:                &row[27],
-			TotalCost:              parseFloat(row[28]),
-		}
-		logs = append(logs, log)
+
+	var log *rows.CostAndUsageLog
+
+	// TODO - find a better way of doing this parsing
+	// this works for now, since the csv columns are fixed and ordered
+	log = &rows.CostAndUsageLog{
+		InvoiceID:              &row[0],
+		PayerAccountId:         &row[1],
+		LinkedAccountId:        &row[2],
+		RecordType:             &row[3],
+		RecordID:               &row[4],
+		BillingPeriodStartDate: parseTime(row[5]),
+		BillingPeriodEndDate:   parseTime(row[6]),
+		InvoiceDate:            parseTime(row[7]),
+		PayerAccountName:       &row[8],
+		LinkedAccountName:      &row[9],
+		TaxationAddress:        &row[10],
+		PayerPONumber:          &row[11],
+		ProductCode:            &row[12],
+		ProductName:            &row[13],
+		SellerOfRecord:         &row[14],
+		UsageType:              &row[15],
+		Operation:              &row[16],
+		RateId:                 &row[17],
+		ItemDescription:        &row[18],
+		UsageStartDate:         parseTime(row[19]),
+		UsageEndDate:           parseTime(row[20]),
+		UsageQuantity:          parseFloat(row[21]),
+		BlendedRate:            &row[22],
+		CurrencyCode:           &row[23],
+		CostBeforeTax:          parseFloat(row[24]),
+		Credits:                parseFloat(row[25]),
+		TaxAmount:              parseFloat(row[26]),
+		TaxType:                &row[27],
+		TotalCost:              parseFloat(row[28]),
 	}
-	slog.Debug("<< logs", "...", logs)
-	return logs, nil
+
+	return log, nil
 }
 
 func parseTime(value string) *time.Time {
