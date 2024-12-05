07a7839977806a654e2a9121315ffcf179a81226
Graza <code@graza.io>
2024-12-05 08:59:05 +0000

    Added validation for S3 Bucket Source configuration. Closes #22 (#38)

--- sources/aws_s3_bucket_source_config.go
+++ sources/aws_s3_bucket_source_config.go
@@ -1,6 +1,9 @@
 package sources
 
 import (
+	"fmt"
+	"regexp"
+
 	"github.com/hashicorp/hcl/v2"
 
 	"github.com/turbot/tailpipe-plugin-sdk/artifact_source_config"
@@ -12,21 +15,45 @@ type AwsS3BucketSourceConfig struct {
 	// required to allow partial decoding
 	Remain hcl.Body `hcl:",remain" json:"-"`
 
-	Bucket        string   `hcl:"bucket"`
-	Prefix        string   `hcl:"prefix"`
-	Extensions    []string `hcl:"extensions"`
-	Region        *string  `hcl:"region"`
-	StartAfterKey *string  `hcl:"start_after_key"`
-
-	// TODO #config better naming
-	LexicographicalOrder bool `hcl:"lexicographical_order,optional"`
+	Bucket               string   `hcl:"bucket"`
+	Prefix               *string  `hcl:"prefix"`
+	Extensions           []string `hcl:"extensions"`
+	Region               *string  `hcl:"region"`
+	StartAfterKey        *string  `hcl:"start_after_key"`
+	LexicographicalOrder bool     `hcl:"lexicographical_order,optional"`
 }
 
-func (c AwsS3BucketSourceConfig) Validate() error {
-	//TODO #config validate the config https://github.com/turbot/tailpipe-plugin-sdk/issues/9
+func (c *AwsS3BucketSourceConfig) Validate() error {
+	if c.Bucket == "" {
+		return fmt.Errorf("bucket is required and cannot be empty")
+	}
+
+	if len(c.Extensions) > 0 {
+		for _, ext := range c.Extensions {
+			if ext == "" {
+				return fmt.Errorf("extensions cannot be empty")
+			}
+			if ext[0] != '.' {
+				return fmt.Errorf("extensions must start with a dot")
+			}
+		}
+	}
+
+	if c.Region != nil && !isValidAWSRegion(*c.Region) {
+		return fmt.Errorf("invalid AWS region '%s'", *c.Region)
+	}
+
 	return nil
 }
 
-func (AwsS3BucketSourceConfig) Identifier() string {
+func (c *AwsS3BucketSourceConfig) Identifier() string {
 	return AwsS3BucketSourceIdentifier
+}
+
+// IsValidAWSRegion checks if the given region is in the FORMAT of an AWS region.
+func isValidAWSRegion(region string) bool {
+	// TODO: #refactor should we ensure it is an actual AWS region via CLI/API?
+	pattern := `^(us|eu|ap|af|me|sa|ca)-(west|east|central|south|north|northeast|southeast|northwest|southwest)-\d+$`
+	re := regexp.MustCompile(pattern)
+	return re.MatchString(region)
 }
