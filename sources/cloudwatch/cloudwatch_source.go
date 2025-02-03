736c177abeeffd19fce573555e0cf786d921bdd6
ParthaI <partha@turbot.com>
2025-02-03 14:16:15 +0530

    Restore CloudWatch source and update compatibility with the latest SDK version

---
+++ sources/cloudwatch/cloudwatch_source.go
@@ -0,0 +1,184 @@
+package cloudwatch
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
+	cwtypes "github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs/types"
+
+	"github.com/turbot/tailpipe-plugin-aws/config"
+	"github.com/turbot/tailpipe-plugin-sdk/collection_state"
+	"github.com/turbot/tailpipe-plugin-sdk/row_source"
+	"github.com/turbot/tailpipe-plugin-sdk/schema"
+	"github.com/turbot/tailpipe-plugin-sdk/types"
+)
+
+const (
+	AwsCloudwatchSourceIdentifier = "aws_cloudwatch_log_group"
+	defaultCloudwatchRegion       = "us-east-1"
+)
+
+// register the source from the package init function
+func init() {
+	row_source.RegisterRowSource[*AwsCloudWatchSource]()
+}
+
+// AwsCloudWatchSource is responsible for collection of events from log streams within a log group in AWS CloudWatch
+type AwsCloudWatchSource struct {
+	row_source.RowSourceImpl[*AwsCloudWatchSourceConfig, *config.AwsConnection]
+}
+
+func (s *AwsCloudWatchSource) Init(ctx context.Context, params *row_source.RowSourceParams, opts ...row_source.RowSourceOption) error {
+	slog.Info("Initializing AwsCloudwatchSource")
+	// set the collection state ctor
+
+	s.NewCollectionStateFunc = collection_state.NewTimeRangeCollectionState
+
+	return s.RowSourceImpl.Init(ctx, params, opts...)
+}
+
+func (s *AwsCloudWatchSource) Identifier() string {
+	return AwsCloudwatchSourceIdentifier
+}
+
+func (s *AwsCloudWatchSource) Collect(ctx context.Context) error {
+
+	// obtain log streams which have active events in the time range
+	logStreamCollection, err := s.collectLogStreams(ctx, s.Config.LogGroupName, s.Config.LogStreamPrefix)
+	if err != nil {
+		return fmt.Errorf("failed to collect log streams, %w", err)
+	}
+
+	// collect events from each log stream
+	for _, ls := range logStreamCollection {
+		sourceEnrichmentFields := &schema.SourceEnrichment{
+			CommonFields: schema.CommonFields{
+				TpSourceType:     AwsCloudwatchSourceIdentifier,
+				TpSourceName:     &s.Config.LogGroupName,
+				TpSourceLocation: ls.LogStream.LogStreamName,
+			},
+		}
+
+		// Get client
+		client, err := s.getClient(ctx)
+		if err != nil {
+			return err
+		}
+
+		// To ensure smoother execution, we have set the value to 7000, even though the maximum allowable limit is 10000.
+		// https://docs.aws.amazon.com/AmazonCloudWatchLogs/latest/APIReference/API_GetLogEvents.html#API_GetLogEvents_RequestSyntax
+		var pageSize int32 = 7000
+		var nextToken *string
+
+		startTime := s.FromTime.Unix()
+
+		input := &cloudwatchlogs.GetLogEventsInput{
+			LogGroupName:  &s.Config.LogGroupName,
+			LogStreamName: ls.LogStream.LogStreamName,
+			StartFromHead: aws.Bool(true),
+			StartTime:     &startTime,
+			Limit:         &pageSize,
+		}
+
+		paginator := cloudwatchlogs.NewGetLogEventsPaginator(client, input)
+		for paginator.HasMorePages() {
+			var output *cloudwatchlogs.GetLogEventsOutput
+			output, err = paginator.NextPage(ctx)
+			if err != nil {
+				return fmt.Errorf("failed to get log events, %w", err)
+			}
+
+			for _, event := range output.Events {
+				if event.Message == nil || *event.Message == "" {
+					continue
+				}
+
+				row := &types.RowData{
+					Data:             event.Message,
+					SourceEnrichment: sourceEnrichmentFields,
+				}
+
+				// update collection state
+				// build time from unix mill
+				unixMillis := *event.Timestamp
+				timestamp := time.Unix(0, unixMillis*int64(time.Millisecond))
+
+				err := s.CollectionState.OnCollected(*ls.LogStream.LogStreamName, timestamp)
+				if err != nil {
+					return fmt.Errorf("failed to update collection state, %w", err)
+				}
+
+				if err := s.OnRow(ctx, row); err != nil {
+					return fmt.Errorf("error processing row: %w", err)
+				}
+			}
+
+			if nextToken != nil && output.NextForwardToken != nil && *nextToken == *output.NextForwardToken {
+				break
+			}
+
+			nextToken = output.NextForwardToken
+		}
+	}
+
+	return nil
+}
+
+// Get log steams for the specified log group
+func (s *AwsCloudWatchSource) collectLogStreams(ctx context.Context, logGroupName string, logStreamPrefix *string) ([]logStreamsToCollect, error) {
+	var logStreams []logStreamsToCollect
+	var nextToken *string
+
+	// Get client
+	client, err := s.getClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	for {
+		input := &cloudwatchlogs.DescribeLogStreamsInput{
+			LogGroupName:        &logGroupName,
+			LogStreamNamePrefix: logStreamPrefix,
+			NextToken:           nextToken,
+		}
+
+		output, err := client.DescribeLogStreams(ctx, input)
+		if err != nil {
+			return nil, fmt.Errorf("failed to describe log streams, %w", err)
+		}
+
+		for _, logStream := range output.LogStreams {
+			logStreams = append(logStreams, logStreamsToCollect{LogStream: logStream})
+		}
+
+		if output.NextToken == nil {
+			break
+		}
+		nextToken = output.NextToken
+	}
+
+	return logStreams, nil
+}
+
+func (s *AwsCloudWatchSource) getClient(ctx context.Context) (*cloudwatchlogs.Client, error) {
+	tempRegion := defaultCloudwatchRegion
+	if s.Config != nil && s.Config.Region != nil {
+		tempRegion = *s.Config.Region
+	}
+
+	cfg, err := s.Connection.GetClientConfiguration(ctx, &tempRegion)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get client configuration, %w", err)
+	}
+
+	client := cloudwatchlogs.NewFromConfig(*cfg)
+	return client, nil
+}
+
+type logStreamsToCollect struct {
+	LogStream cwtypes.LogStream
+}
--- tables/cloudtrail_log/cloudtrail_log_table.go
+++ tables/cloudtrail_log/cloudtrail_log_table.go
@@ -2,6 +2,7 @@ package cloudtrail_log
 
 import (
 	"github.com/turbot/tailpipe-plugin-aws/sources/s3_bucket"
+	"github.com/turbot/tailpipe-plugin-aws/sources/cloudwatch"
 	"github.com/turbot/tailpipe-plugin-aws/tables"
 	"strings"
 	"time"
@@ -42,6 +43,10 @@ func (t *CloudTrailLogTable) GetSourceMetadata() []*table.SourceMetadata[*CloudT
 			Options: []row_source.RowSourceOption{
 				artifact_source.WithArtifactExtractor(NewCloudTrailLogExtractor()),
 			},
+		},
+		{
+			SourceName: cloudwatch.AwsCloudwatchSourceIdentifier,
+			Mapper:     &CloudTrailMapper{},
 		},
 	}
 }
