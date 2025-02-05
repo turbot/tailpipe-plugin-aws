3ca4c88316f1a5dd49a3bdd290b2e6800315dc58
Priyanka Chatterjee <priyanka.chatterjee@turbot.com>
2025-02-05 15:30:34 +0530

    Add table s3 server access log table

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/turbot/tailpipe-plugin-aws
 
 go 1.23.2
 
-require github.com/turbot/tailpipe-plugin-sdk v0.1.0
+replace github.com/turbot/tailpipe-plugin-sdk => ../tailpipe-plugin-sdk
 
 require (
 	cloud.google.com/go v0.115.0 // indirect
@@ -66,6 +66,7 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.13.0 // indirect
+	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-getter v1.7.5 // indirect
@@ -91,6 +92,7 @@ require (
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/jtolds/gls v4.20.0+incompatible // indirect
 	github.com/karrick/gows v0.3.0 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -117,9 +119,11 @@ require (
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/satyrius/gonx v1.4.0 // indirect
 	github.com/shiena/ansicolor v0.0.0-20230509054315-a9deabde6e02 // indirect
 	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/smarty/assertions v1.16.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
@@ -184,5 +188,6 @@ require (
 	github.com/rs/xid v1.5.0
 	github.com/turbot/go-kit v0.10.0-rc.0
 	github.com/turbot/pipe-fittings/v2 v2.0.0
+	github.com/turbot/tailpipe-plugin-sdk v0.0.0-00010101000000-000000000000
 	golang.org/x/sync v0.10.0
 )
---
+++ tables/s3_server_access_log/s3_server_access_log.go
@@ -0,0 +1,170 @@
+package s3_server_access_log
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/turbot/tailpipe-plugin-sdk/schema"
+)
+
+type S3ServerAccessLog struct {
+	schema.CommonFields
+
+	BucketOwner        string    `json:"bucket_owner"`
+	Bucket             string    `json:"bucket"`
+	Timestamp          time.Time `json:"timestamp"`
+	RemoteIP           string    `json:"remote_ip"`
+	Requester          string    `json:"requester"`
+	RequestID          string    `json:"request_id"`
+	Operation          string    `json:"operation"`
+	Key                *string   `json:"key,omitempty"`
+	RequestURI         *string   `json:"request_uri,omitempty"`
+	HTTPStatus         *int      `json:"http_status,omitempty"`
+	ErrorCode          *string   `json:"error_code,omitempty"`
+	BytesSent          *int64    `json:"bytes_sent,omitempty"`
+	ObjectSize         *int64    `json:"object_size,omitempty"`
+	TotalTime          *int      `json:"total_time,omitempty"`
+	TurnAroundTime     *int      `json:"turn_around_time,omitempty"`
+	Referer            *string   `json:"referer,omitempty"`
+	UserAgent          *string   `json:"user_agent,omitempty"`
+	VersionID          *string   `json:"version_id,omitempty"`
+	HostID             *string   `json:"host_id,omitempty"`
+	SignatureVersion   *string   `json:"signature_version,omitempty"`
+	CipherSuite        *string   `json:"cipher_suite,omitempty"`
+	AuthenticationType *string   `json:"authentication_type,omitempty"`
+	HostHeader         *string   `json:"host_header,omitempty"`
+	TLSVersion         *string   `json:"tls_version,omitempty"`
+	AccessPointArn     *string   `json:"access_point_arn,omitempty"`
+	AclRequired        *bool     `json:"acl_required,omitempty"`
+}
+
+func NewS3ServerAccessLog() *S3ServerAccessLog {
+	return &S3ServerAccessLog{}
+}
+
+func (l *S3ServerAccessLog) InitialiseFromMap(m map[string]string) error {
+	for key, value := range m {
+		switch key {
+		case "bucket_owner":
+			l.BucketOwner = value
+		case "bucket":
+			l.Bucket = value
+		case "timestamp":
+			ts, err := time.Parse("02/Jan/2006:15:04:05 -0700", value)
+			if err != nil {
+				return fmt.Errorf("error parsing timestamp: %w", err)
+			}
+			l.Timestamp = ts
+		case "remote_ip":
+			l.RemoteIP = value
+		case "requester":
+			l.Requester = value
+		case "request_id":
+			l.RequestID = value
+		case "operation":
+			l.Operation = value
+		case "key":
+			if value != "-" {
+				l.Key = &value
+			}
+		case "request_uri":
+			if value != "-" {
+				l.RequestURI = &value
+			}
+		case "http_status":
+			if value != "-" {
+				hs, err := strconv.Atoi(value)
+				if err != nil {
+					return fmt.Errorf("error parsing http_status: %w", err)
+				}
+				l.HTTPStatus = &hs
+			}
+		case "error_code":
+			if value != "-" {
+				l.ErrorCode = &value
+			}
+		case "bytes_sent":
+			if value != "-" {
+				bs, err := strconv.ParseInt(value, 10, 64)
+				if err != nil {
+					return fmt.Errorf("error parsing bytes_sent: %w", err)
+				}
+				l.BytesSent = &bs
+			}
+		case "object_size":
+			if value != "-" {
+				os, err := strconv.ParseInt(value, 10, 64)
+				if err != nil {
+					return fmt.Errorf("error parsing object_size: %w", err)
+				}
+				l.ObjectSize = &os
+			}
+		case "total_time":
+			if value != "-" {
+				tt, err := strconv.Atoi(value)
+				if err != nil {
+					return fmt.Errorf("error parsing total_time: %w", err)
+				}
+				l.TotalTime = &tt
+			}
+		case "turn_around_time":
+			if value != "-" {
+				tat, err := strconv.Atoi(value)
+				if err != nil {
+					return fmt.Errorf("error parsing turn_around_time: %w", err)
+				}
+				l.TurnAroundTime = &tat
+			}
+		case "referer":
+			if value != "-" {
+				l.Referer = &value
+			}
+		case "user_agent":
+			if value != "-" {
+				l.UserAgent = &value
+			}
+		case "version_id":
+			if value != "-" {
+				l.VersionID = &value
+			}
+		case "host_id":
+			if value != "-" {
+				l.HostID = &value
+			}
+		case "signature_version":
+			if value != "-" {
+				l.SignatureVersion = &value
+			}
+		case "cipher_suite":
+			if value != "-" {
+				l.CipherSuite = &value
+			}
+		case "authentication_type":
+			if value != "-" {
+				l.AuthenticationType = &value
+			}
+		case "host_header":
+			if value != "-" {
+				l.HostHeader = &value
+			}
+		case "tls_version":
+			if value != "-" {
+				l.TLSVersion = &value
+			}
+		case "access_point_arn":
+			if value != "-" {
+				l.AccessPointArn = &value
+			}
+		case "acl_required":
+			if value != "-" {
+				b := true
+				l.AclRequired = &b
+			} else {
+				b := false
+				l.AclRequired = &b
+			}
+		}
+	}
+	return nil
+}
