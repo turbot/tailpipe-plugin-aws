b6fbd9b1f579b17c0fc8fbad6d3de6959d051129
Priyanka Chatterjee <priyanka.chatterjee@turbot.com>
2025-02-20 19:53:37 +0530

    Initial table

---
+++ tables/nlb_access_log/nlb_access_log.go
@@ -0,0 +1,116 @@
+package nlb_access_log
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/turbot/tailpipe-plugin-sdk/schema"
+)
+
+type NlbAccessLogBatch struct {
+	Records []NlbAccessLog `json:"Records"`
+}
+
+type NlbAccessLog struct {
+	schema.CommonFields
+
+	Type                      string    `json:"type"`
+	Version                   string    `json:"version"`
+	Time                      time.Time `json:"time"`
+	Elb                       string    `json:"elb"`
+	Listener                  string    `json:"listener"`
+	ClientIP                  string    `json:"client_ip"`
+	ClientPort                int       `json:"client_port"`
+	DestinationIP             string    `json:"destination_ip"`
+	DestinationPort           int       `json:"destination_port"`
+	ConnectionTime            int       `json:"connection_time"`
+	TLSHandshakeTime          int       `json:"tls_handshake_time"`
+	ReceivedBytes             int64     `json:"received_bytes"`
+	SentBytes                 int64     `json:"sent_bytes"`
+	IncomingTLSAlert          string    `json:"incoming_tls_alert,omitempty"`
+	ChosenCertArn             string    `json:"chosen_cert_arn,omitempty"`
+	ChosenCertSerial          string    `json:"chosen_cert_serial,omitempty"`
+	TLSCipher                 string    `json:"tls_cipher,omitempty"`
+	TLSProtocolVersion        string    `json:"tls_protocol_version,omitempty"`
+	TLSNamedGroup             string    `json:"tls_named_group,omitempty"`
+	DomainName                string    `json:"domain_name,omitempty"`
+	ALPNFEProtocol            string    `json:"alpn_fe_protocol,omitempty"`
+	ALPNBEProtocol            string    `json:"alpn_be_protocol,omitempty"`
+	ALPNClientPreferenceList  string    `json:"alpn_client_preference_list,omitempty"`
+	TLSConnectionCreationTime time.Time `json:"tls_connection_creation_time"`
+}
+
+// InitialiseFromMap - initialise the struct from a map
+func (l *NlbAccessLog) InitialiseFromMap(m map[string]string) error {
+	var err error
+	for key, value := range m {
+		if value == "-" {
+			continue
+		}
+		switch key {
+		case "time":
+			l.Time, err = time.Parse(time.RFC3339, value)
+			if err != nil {
+				return fmt.Errorf("error parsing time: %w", err)
+			}
+		case "type":
+			l.Type = value
+		case "version":
+			l.Version = value
+		case "elb":
+			l.Elb = value
+		case "listener":
+			l.Listener = value
+		case "client":
+			parts := strings.Split(value, ":")
+			l.ClientIP = parts[0]
+			l.ClientPort, err = strconv.Atoi(parts[1])
+			if err != nil {
+				return fmt.Errorf("error parsing client_port: %w", err)
+			}
+		case "destination":
+			parts := strings.Split(value, ":")
+			l.DestinationIP = parts[0]
+			l.DestinationPort, err = strconv.Atoi(parts[1])
+			if err != nil {
+				return fmt.Errorf("error parsing destination_port: %w", err)
+			}
+		case "connection_time":
+			l.ConnectionTime, err = strconv.Atoi(value)
+			if err != nil {
+				return fmt.Errorf("error parsing connection_time: %w", err)
+			}
+		case "tls_handshake_time":
+			l.TLSHandshakeTime, err = strconv.Atoi(value)
+			if err != nil {
+				return fmt.Errorf("error parsing tls_handshake_time: %w", err)
+			}
+		case "received_bytes":
+			l.ReceivedBytes, err = strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				return fmt.Errorf("error parsing received_bytes: %w", err)
+			}
+		case "sent_bytes":
+			l.SentBytes, err = strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				return fmt.Errorf("error parsing sent_bytes: %w", err)
+			}
+		case "chosen_cert_arn":
+			l.ChosenCertArn = value
+		case "tls_cipher":
+			l.TLSCipher = value
+		case "tls_protocol_version":
+			l.TLSProtocolVersion = value
+		case "domain_name":
+			l.DomainName = value
+		case "tls_connection_creation_time":
+			l.TLSConnectionCreationTime, err = time.Parse(time.RFC3339, value)
+			if err != nil {
+				return fmt.Errorf("error parsing tls_connection_creation_time: %w", err)
+			}
+		}
+	}
+	return nil
+}
---
+++ tables/nlb_access_log/nlb_access_log_table.go
@@ -0,0 +1,86 @@
+package nlb_access_log
+
+import (
+	"strings"
+	"time"
+
+	"github.com/rs/xid"
+	"github.com/turbot/pipe-fittings/v2/utils"
+	"github.com/turbot/tailpipe-plugin-aws/sources/s3_bucket"
+	"github.com/turbot/tailpipe-plugin-sdk/artifact_source"
+	"github.com/turbot/tailpipe-plugin-sdk/artifact_source_config"
+	"github.com/turbot/tailpipe-plugin-sdk/constants"
+	"github.com/turbot/tailpipe-plugin-sdk/mappers"
+	"github.com/turbot/tailpipe-plugin-sdk/row_source"
+	"github.com/turbot/tailpipe-plugin-sdk/schema"
+	"github.com/turbot/tailpipe-plugin-sdk/table"
+)
+
+const NlbAccessLogTableIdentifier = "aws_nlb_access_log"
+
+func init() {
+	table.RegisterTable[*NlbAccessLog, *NlbAccessLogTable]()
+}
+
+const nlbLogFormat = `$type $version $time $elb $listener $client_port $destination_port $connection_time $tls_handshake_time $received_bytes $sent_bytes "$incoming_tls_alert" "$chosen_cert_arn" "$chosen_cert_serial" "$tls_cipher" "$tls_protocol_version" "$tls_named_group" "$domain_name" "$alpn_fe_protocol" "$alpn_be_protocol" "$alpn_client_preference_list" $tls_connection_creation_time`
+
+type NlbAccessLogTable struct{}
+
+func (c *NlbAccessLogTable) Identifier() string {
+	return NlbAccessLogTableIdentifier
+}
+
+func (c *NlbAccessLogTable) GetSourceMetadata() []*table.SourceMetadata[*NlbAccessLog] {
+	defaultS3ArtifactConfig := &artifact_source_config.ArtifactSourceConfigImpl{
+		FileLayout: utils.ToStringPointer("AWSLogs/(%{DATA:org_id}/)?%{NUMBER:account_id}/elasticloadbalancing/%{DATA:region}/%{YEAR:year}/%{MONTHNUM:month}/%{MONTHDAY:day}/%{DATA}.log.gz"),
+	}
+
+	return []*table.SourceMetadata[*NlbAccessLog]{
+		{
+			SourceName: s3_bucket.AwsS3BucketSourceIdentifier,
+			Mapper:     mappers.NewGonxMapper[*NlbAccessLog](nlbLogFormat),
+			Options: []row_source.RowSourceOption{
+				artifact_source.WithDefaultArtifactSourceConfig(defaultS3ArtifactConfig),
+				artifact_source.WithRowPerLine(),
+			},
+		},
+		{
+			SourceName: constants.ArtifactSourceIdentifier,
+			Mapper:     mappers.NewGonxMapper[*NlbAccessLog](nlbLogFormat),
+			Options: []row_source.RowSourceOption{
+				artifact_source.WithRowPerLine(),
+			},
+		},
+	}
+}
+
+func (c *NlbAccessLogTable) EnrichRow(row *NlbAccessLog, sourceEnrichmentFields schema.SourceEnrichment) (*NlbAccessLog, error) {
+	row.CommonFields = sourceEnrichmentFields.CommonFields
+
+	row.TpID = xid.New().String()
+	row.TpIngestTimestamp = time.Now()
+	row.TpTimestamp = row.Time
+	row.TpDate = row.Time.Truncate(24 * time.Hour)
+	row.TpIndex = strings.TrimPrefix(row.Elb, "net/")
+
+	row.TpSourceIP = &row.ClientIP
+	row.TpIps = append(row.TpIps, row.ClientIP)
+	if row.DestinationIP != "" {
+		row.TpDestinationIP = &row.DestinationIP
+		row.TpIps = append(row.TpIps, row.DestinationIP)
+	}
+
+	if row.DomainName != "" {
+		row.TpDomains = append(row.TpDomains, row.DomainName)
+	}
+
+	if row.ChosenCertArn != "" {
+		row.TpAkas = append(row.TpAkas, row.ChosenCertArn)
+	}
+
+	return row, nil
+}
+
+func (c *NlbAccessLogTable) GetDescription() string {
+	return "AWS NLB Access logs capture detailed information about the connections that pass through a Network Load Balancer. This table provides a structured representation of the log data, including TLS handshake details, client and destination information, connection duration, and security parameters."
+}
